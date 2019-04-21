ce1145a5ccde72affc126450eac5759815058e0e
Emmanuel Touzery <etouzery@gmail.com>
2019-04-21 23:21:49 +0200

    list of pipes instead of hardcoding them. some tuning of the numbers to look nicer

--- spiral-leap.rkt
+++ spiral-leap.rkt
@@ -11,7 +11,7 @@
 (define lights+camera
   (combine (light (pos 0 1 2) (emitted "white" 10))
            (light (pos 0 -1 -2) (emitted "orange" 7))
-           (basis 'camera (point-at (pos 1 1 1) origin))))
+           (basis 'camera (point-at (pos 1 1 .7) origin))))
 
 (define (easing-fn x)
   ;; https://gist.github.com/gre/1650294
@@ -25,11 +25,29 @@
      (remainder (round t) jump-duration)
      jump-duration))
   ;; split the 0-1 range in two ranges (going up then down)
-  (if  (> jump-done-ratio 1/2)
+  ;; then scale it down by 0.7 to fit between the pipes
+  (* 0.7 (if  (> jump-done-ratio 1/2)
        ;; going up.. transform 0.5->1 into 0->1 then apply the easing fn
        (easing-fn (* 2 (- jump-done-ratio 1/2)))
        ;; going down.. transform 0->0.5 into 0->1 then 1->0 then apply the easing fn
-       (easing-fn (- 1 (* jump-done-ratio 2)))))
+       (easing-fn (- 1 (* jump-done-ratio 2))))))
+
+(struct pipe-info (rotation-offset) #:transparent)
+
+(define pipes
+  (list
+   (pipe-info 30)
+   (pipe-info 70)
+   (pipe-info 140)))
+
+(define (render-pipe cur-pipe idx)
+  ;; times 0.9 it looks nicer like that
+  (define v-offset* (* (- idx 1) .9))
+  (rotate-z (pipe
+             (pos -1/2 -1/2 (- -1/15 v-offset*))
+             (pos 1/2 1/2 (- 1/15 v-offset*))
+             #:arc (arc 90 360))
+            (pipe-info-rotation-offset cur-pipe)))
 
 (define (on-draw s n t)
   (combine
@@ -40,27 +58,11 @@
         (rgba "green" 1)
       (move-z (sphere (pos 1/4 1/4 0) 1/15) (ball-position-z t))))
 
-   ;; cevka 1
-   (rotate-z (pipe
-              (pos -1/2 -1/2 -1/15)
-              (pos 1/2 1/2 1/15)
-              #:arc (arc 90 360))
-             (+ s 70))
+   (for/list ([pipe pipes]
+              [idx (in-naturals)])
+     (rotate-z (render-pipe pipe idx) s))
+    
    (cylinder (pos -1/3 -1/3 -4) (pos 1/8 1/8 2))
-   
-   ;; cevka 2
-   (rotate-z (pipe
-              (pos -1/2 -1/2 -16/15)
-              (pos 1/2 1/2 -18/15)
-              #:arc (arc 90 360))
-             (+ s 17))
-   
-   ;; cevka 3
-   (rotate-z (pipe
-              (pos -1/2 -1/2 10/15)
-              (pos 1/2 1/2 8/15)
-              #:arc (arc 90 360))
-             (+ s 173))
 
    ;; cilinder na sredini
    (cylinder (pos -1/3 -1/3 -4) (pos 1/8 1/8 2))
